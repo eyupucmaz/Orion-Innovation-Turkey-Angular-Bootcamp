56d87bf181efd6b58687a456a44db3c99626b70f
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-02-20 15:41:12 +0300

    add approach 1 and tested with inputs

--- Week-1/part1/main.js
+++ Week-1/part1/main.js
@@ -1,14 +1,31 @@
 /*
   input : an array of numbers [1,2,3,4,5]
   target : a number
-  output : returns the indexes of pairs whose sum is equal to the target
+  output : returns the indices of pairs whose sum is equal to the target
 */
 
-let input = [2,5,7,11,15]; // Input array
+let input = [2, 7, 3, 15, 6, 3]; // Input array
 let target = 9; // target number
 
+// Approach-1
+const findPairs = (input, target) => {
+	let pairs = []; // array to store the pairs
+	for (let i = 0; i < input.length; i++) {
+		// loop through the input array
+		for (let j = i + 1; j < input.length; j++) {
+			if (input[i] + input[j] === target) {
+				// if sum of two equals target and push in the pairs
+				pairs.push([i, j]);
+			}
+		}
+	}
+	return pairs; // return the indices
+};
 
+/*
+  time complexity: n*n = O(n^2)
+*/
 
-
+console.log(findPairs(input, target));
 
 
