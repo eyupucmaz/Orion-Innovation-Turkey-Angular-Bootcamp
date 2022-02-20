52127860e97046067c7978d176334e29b8f892c8
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-02-20 14:59:59 +0300

    Add description about question and inputs

---
+++ Week-1/part1/main.js
@@ -0,0 +1,14 @@
+/*
+  input : an array of numbers [1,2,3,4,5]
+  target : a number
+  output : returns the indexes of pairs whose sum is equal to the target
+*/
+
+let input = [2,5,7,11,15]; // Input array
+let target = 9; // target number
+
+
+
+
+
+
