7e23a4edf4a3f62b4f04cd76f1159234d16f945c
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-02-20 17:28:04 +0300

    Add second approach

--- Week-1/part1/main.js
+++ Week-1/part1/main.js
@@ -4,7 +4,7 @@
   output : returns the indices of pairs whose sum is equal to the target
 */
 
-let input = [2, 7, 3, 15, 6, 3]; // Input array
+let input = [2,7,11,15,6,3,4.5,4.5]; // Input array
 let target = 9; // target number
 
 // Approach-1
@@ -28,4 +28,34 @@ const findPairs = (input, target) => {
 
 console.log(findPairs(input, target));
 
+// Approach-2
+function findPairsTwo(input, target) {
+	let pairs = []; // array to store the pairs
+	let remain = 0; // number should find in input array
+	// loop through the input array
+	for (let i = 0; i < input.length; i++) {
+		// target - a = b
+		remain = target - input[i];
+		// if there is a number
+		if (input.indexOf(remain) !== -1) {
+      // if index of remain is not equal to current index
+			if (i !== input.indexOf(remain)) {
+        // push the indices to pairs
+				pairs.push([i, input.indexOf(remain)]);
+      } else {
+        // otherwise find the last index of number
+				pairs.push([input.indexOf(input[i]), input.lastIndexOf(input[i])]);
+			}
+		}
+	}
+	return pairs;
+}
+/*
+  time complexity: n times loop ==> O(n)
+*/
 
+/*
+  Issue: the second approach returns indeces two times ??
+*/
+
+console.log(findPairsTwo(input, target));
